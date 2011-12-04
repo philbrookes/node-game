67e7c33b25905f75d72959fa31832ae69d20fadd
Craig Brookes <craigbrookes@gmail.com>
2011-12-04 22:00:21 +0000

    removing connections on disconnect

--- example.js
+++ example.js
@@ -4,12 +4,24 @@ Most awesome branch ever
 
 */
 var net = require('net');
-var connections = [];
+var connections = {};
 var server = net.createServer(function (socket) {
-   connections.push(socket);
+  var _id = Math.random(0,1000);
+  connections[_id] = socket;
+  socket._connid = _id;
   socket.setEncoding("UTF-8");
   socket.on('data',function(data){
        console.log(data);
+  });
+  socket.on('end',function(){
+       for(theid in connections){
+           if(this._connid == theid){
+               console.log("the id in connecitons = "+ theid + "this sockets id = "+ this._connid);
+               delete connections[theid];
+               break;
+           } 
+      } 
+      console.log(connections);
   });
   socket.write("Echo awesome server\r\n");
   socket.pipe(socket);
