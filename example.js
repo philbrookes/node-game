327d8c203c3a6ba1f9e30333d76cc26dcb82531e
phil brookes <phil@mywebdesign.ie>
2011-12-04 22:16:14 +0000

    parsing data input

--- example.js
+++ example.js
@@ -6,15 +6,16 @@ Most awesome branch ever
 var net = require('net');
 var connections = [];
 var server = net.createServer(function (socket) {
-   connections.push(socket);
-  socket.setEncoding("UTF-8");
-  socket.on('data',function(data){
-       console.log(data);
+	connections.push(socket);
+	socket.setEncoding("UTF-8");
+	socket.on('data',function(data){
+		console.log(data);
+		for(connId in connections){
+            data = data.replace("\r\n", "");
+            connections[connId].write(data + " started with: "+data.charAt(0));
+		}
   });
-  socket.write("Welcome to the chat room!\r\n");
-	for(connId in connections){
-		socket.pipe(connections[connId]);
-	}
+  
 });
 
 
