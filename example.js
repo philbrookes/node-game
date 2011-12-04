f84a405df85839ae739ad5de7713ec31abdd683e
Craig Brookes <craigbrookes@gmail.com>
2011-12-04 21:47:29 +0000

    connectionrefs

--- example.js
+++ example.js
@@ -4,14 +4,17 @@ Most awesome branch ever
 
 */
 var net = require('net');
-
+var connections = [];
 var server = net.createServer(function (socket) {
+   connections.push(socket);
   socket.setEncoding("UTF-8");
   socket.on('data',function(data){
        console.log(data);
   });
   socket.write("Echo awesome server\r\n");
   socket.pipe(socket);
+  console.log(connections);
 });
+
 
 server.listen(13378, "127.0.0.1");
